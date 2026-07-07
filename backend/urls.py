2698fffb2742b78808bdced75e8d56b542766c29
Aimal Mazhar <aimalmazhar526@gmail.com>
2026-07-07 11:17:12 +0500

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello! API is working. Visit /admin/ or /api/")
 
 urlpatterns = [
+    path('', home),  # Add this line
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),
 ]
