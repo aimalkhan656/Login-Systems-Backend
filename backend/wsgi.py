db8950ca0e26e9b51abcef70a1f61a0f15456a7b
Aimal Mazhar <aimalmazhar526@gmail.com>
2026-07-07 10:40:01 +0500

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,6 +1,9 @@
 import os
+import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
